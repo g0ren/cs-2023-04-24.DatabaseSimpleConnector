63fe3a6ddb5d1d12e42ce0722c40488a1e99b3a1
g0ren <g0ren@mail.ru>
2023-04-23 00:05:41 +0300

    Add project files.

---
+++ DatabaseTest/MySqlDatabaseConnector.cs
@@ -0,0 +1,57 @@
+﻿using MySql.Data.MySqlClient;
+using System.Text;
+
+namespace DataBaseTest
+{
+    class MySqlDatabaseConnector : IDisposable
+    {
+        MySqlConnection? _connection;
+        public MySqlDatabaseConnector(string server, string database, string userid, string password)
+        {
+            MySqlConnectionStringBuilder mySqlConnectionStringBuilder = new MySqlConnectionStringBuilder()
+            {
+                Server = server,
+                Database = database,
+                UserID = userid,
+                Password = password
+            };
+
+            _connection = new MySqlConnection(mySqlConnectionStringBuilder.ToString());
+        }
+
+        public async Task<string[]> SendQuery(string query)
+        {
+            MySqlCommand cmd = new MySqlCommand(query, _connection);
+            string[] response = new string[] { };
+            _connection?.OpenAsync();
+            using (MySqlDataReader reader = await cmd.ExecuteReaderAsync() as MySqlDataReader)
+            {
+                StringBuilder builder = new StringBuilder();
+                while (await reader.ReadAsync())
+                {
+                    for (int i = 0; i < reader.FieldCount; i++)
+                    {
+                        builder.Append(reader.GetString(i));
+                        builder.Append('\t');
+                    }
+                    response = response.Append(builder.ToString()).ToArray();
+                    builder.Clear();
+                }
+            }
+            _ = Close();
+            return response;
+        } 
+
+        public async Task Close()
+        {
+            if (_connection != null)
+                await _connection.CloseAsync();
+        }
+
+        public void Dispose()
+        {
+            _ = Close();
+        }
+
+    }
+}
---
+++ DatabaseTest/Program.cs
@@ -0,0 +1,100 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace DataBaseTest
+{
+
+    class Program
+    {
+        // Credentials
+        static string server = "db4free.net";
+        static string database = "mikhail_db";
+        static string userid = "mikhail";
+        static string password = "S8nluvzu";
+
+        static async Task<string[]> MakeQuery(string query)
+        {
+            using (MySqlDatabaseConnector connector =
+                new MySqlDatabaseConnector(server, database, userid, password))
+            {
+                try
+                {
+                    string[] response = await connector.SendQuery(query);
+                    return response;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                return new string[] { "" };
+            }
+        }
+        static async Task ListCharacters()
+        {
+            string query =
+                @"SELECT c.NAME AS ""Character Name"", cc.NAME AS ""Class Name"" " +
+                "FROM Characters AS c, CharactersClass AS cc " +
+                "WHERE c.CharacterClassId = cc.Id;";
+            string[] response = await MakeQuery(query);
+            foreach (var line in response)
+            {
+                Console.WriteLine(line);
+            }
+        }
+
+        static async Task AddCharacter(string name, string className)
+        {
+            string[] currentClass = await MakeQuery(
+                "SELECT Id " +
+                "FROM CharactersClass " +
+                @"WHERE Name = """ + className + @""";"
+                );
+            if (currentClass.Length == 0)
+            {
+                _ = await MakeQuery("INSERT INTO CharactersClass(Name) " +
+                    @"VALUES (""" + className + @""");"
+                    );
+                currentClass = await MakeQuery(
+                "SELECT Id " +
+                "FROM CharactersClass " +
+                @"WHERE Name = """ + className + @""";"
+                );
+            }
+            _ = await MakeQuery(
+                "INSERT INTO Characters(Name, CharacterClassId) " +
+                @"VALUES (""" +name + @""", " + $"{currentClass[0]});"
+                );
+            
+        }
+
+        public static async Task Main()
+        {
+            int command = 0;
+
+            while (true)
+            {
+                Console.WriteLine("Enter command:");
+                Console.WriteLine("1. List current characters and their classes");
+                Console.WriteLine("2. Add a character");
+                Console.WriteLine("0. Quit");
+
+                command = Convert.ToInt32(Console.ReadLine());
+                switch (command)
+                {
+                    case 1:
+                        await ListCharacters();
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter the character name");
+                        string name = Console.ReadLine();
+                        Console.WriteLine("Enter the character class");
+                        string className = Console.ReadLine();
+                        await AddCharacter(name, className);
+                        break;
+                    case 0:
+                        return;
+                }
+            }
+
+        }
+    }
+}
